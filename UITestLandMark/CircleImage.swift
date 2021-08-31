9679d3914cfcd924022ba4196fbecd98ef8d364e
Niwarthana Nihathamani <niwarthana.2016285@iit.ac.lk>
2021-08-31 15:39:14 +0530

    Initial commit

---
+++ UITestLandMark/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  UITestLandMark
+//
+//  Created by Niwarthana De Alwis on 2021-08-31.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
